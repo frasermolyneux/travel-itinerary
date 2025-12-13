c1d48a6a1c9d7096a906f314b85560facda085f4
Fraser Molyneux <frasermolyneux@mx-mail.io>
2025-12-13 11:16:33 +0000

    Fix resource group references in app_service_plan configuration

--- terraform/app_service_plan.tf
+++ terraform/app_service_plan.tf
@@ -1,7 +1,7 @@
 resource "azurerm_service_plan" "sp" {
   name                = local.app_service_plan_name
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
 
   os_type  = "Linux"
   sku_name = var.app_service_plan.sku
