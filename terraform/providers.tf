fc4189745a1b20daa092a599b603e8d44d9c75f8
Fraser Molyneux <frasermolyneux@mx-mail.io>
2026-02-28 20:55:10 +0000

    fix: Align subscription_id formatting in providers and update dev environment subscription_id

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -20,7 +20,8 @@ terraform {
 }
 
 provider "azurerm" {
-  subscription_id = var.subscription_id
+  subscription_id                 = var.subscription_id
+  resource_provider_registrations = "none"
 
   features {
     resource_group {
@@ -32,8 +33,9 @@ provider "azurerm" {
 }
 
 provider "azurerm" {
-  alias           = "dns"
-  subscription_id = var.dns.subscription_id
+  alias                           = "dns"
+  subscription_id                 = var.dns.subscription_id
+  resource_provider_registrations = "none"
 
   features {}
 
